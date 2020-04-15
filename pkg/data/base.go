2f8f864ad85ceb87aa8f13f6f9cb3fa608e87aac
Luca <lucat1@users.noreply.github.com>
2020-04-15 13:06:21 +0200

    :fire:

--- pkg/data/base.go
+++ pkg/data/base.go
@@ -1,7 +1,6 @@
 package data
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/lucat1/o2/pkg/auth"
@@ -10,7 +9,6 @@ import (
 
 // Base generates the basic data contained in each respose, such as the login status
 var Base Composer = func(r *http.Request) quercia.Props {
-	fmt.Println(r.Cookie("token"))
 	data, isAuthenticated := auth.IsAuthenticated(r)
 	if !isAuthenticated {
 		return quercia.Props{}
