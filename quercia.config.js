bd2437ab1c060d6bfe4b447aafef157e6df889fe
luca <lucat1@users.noreply.github.com>
2020-05-02 11:43:29 +0200

    chore: bump quercia version

--- quercia.config.js
+++ quercia.config.js
@@ -1,7 +1,7 @@
 module.exports = function({ config, target, mode }) {
   if (target === 'client') {
     // include goober in the vendor module as it's used in all pages
-    config.optimization.splitChunks.cacheGroups.vendor.test = /(?<!node_modules.*)[\\/]node_modules[\\/](react|react-dom|scheduler|prop-types|object-assign|preact|@quercia\/(quercia|cli)|core-js|babel-plugin-transform-async-to-promises|react-hot-loader|@hot-loader|tiny-(invariant|warning)|goober)[\\/]/
+    config.optimization.splitChunks.cacheGroups.vendor.test = /(?<!node_modules.*)[\\/]node_modules[\\/](react|react-dom|scheduler|prop-types|object-assign|preact|@quercia\/(quercia|cli)|core-js|babel-plugin-transform-async-to-promises|react-hot-loader|@hot-loader|html-entities|goober)[\\/]/
 
     // for production use `preact` instead of `react` to save bytes
     if (mode === 'production') {
