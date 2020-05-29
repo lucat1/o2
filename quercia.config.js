c96d58ff3eba54efc2da92d36a0ca0d2d18a1ad0
Luca <lucat1@users.noreply.github.com>
2020-05-29 11:03:02 +0200

    chore: bump quercia version, better caching

--- quercia.config.js
+++ quercia.config.js
@@ -1,13 +1,13 @@
 const { join } = require('path')
 
-module.exports = function({ config, target, mode, buildID }) {
+module.exports = function({ config, target, mode }) {
   config.module.rules.push({
     test: /\.(woff2)$/i,
     loader: 'file-loader',
     options: {
       // set a static public path so that both the client
       // and the server can have the same url for the file
-      publicPath: join('__quercia', buildID, 'client')
+      publicPath: join('__quercia', 'client')
     }
   })
 
