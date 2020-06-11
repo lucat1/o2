1672ce3ecd863ece59fa9181b2d31f27c126fe65
Luca <lucat1@users.noreply.github.com>
2020-06-11 12:25:31 +0200

    feat: convert commit page

--- routes/repository/commit.go
+++ routes/repository/commit.go
@@ -9,13 +9,13 @@ import (
 	"github.com/lucat1/o2/pkg/log"
 	"github.com/lucat1/o2/pkg/middleware"
 	"github.com/lucat1/o2/pkg/models"
+	"github.com/lucat1/o2/pkg/render"
 	"github.com/lucat1/o2/routes/datas"
 	"github.com/lucat1/o2/routes/shared"
-	"github.com/lucat1/quercia"
 )
 
-// Commit renders the diff of a single commit
-func Commit(w http.ResponseWriter, r *http.Request) {
+// CommitRenderer returns the page and the render data for the commit page
+var CommitRenderer render.Renderer = func(w http.ResponseWriter, r *http.Request) render.Result {
 	dbRepo := r.Context().Value(middleware.DbRepo).(models.Repository)
 	repo := r.Context().Value(middleware.GitRepo).(*git.Repository)
 	sha := muxie.GetParam(w, "sha")
@@ -28,17 +28,20 @@ func Commit(w http.ResponseWriter, r *http.Request) {
 			Str("sha", sha).
 			Err(err).
 			Msg("Error while looking for commit inside a git repository")
-		shared.NotFound(w, r)
-		return
+
+		return shared.NotFoundRenderer(w, r)
 	}
 
-	quercia.Render(
-		w, r,
-		"repository/commit",
-		data.Compose(r,
-			data.Base,
+	return render.Result{
+		Page: "repository/commit",
+		Composers: []data.Composer{
 			datas.RepositoryData(dbRepo),
 			datas.CommitData(commit),
-		),
-	)
+		},
+	}
+}
+
+// Commit renders the diff of a single commit
+func Commit(w http.ResponseWriter, r *http.Request) {
+	render.Render(w, r, CommitRenderer)
 }
