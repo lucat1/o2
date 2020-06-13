ae90a4e0036a5a33df75a7f88081736fc93e7f98
Luca <lucat1@users.noreply.github.com>
2020-06-13 13:16:53 +0200

    feat: add default OGP tags

--- pkg/render/ogp.go
+++ pkg/render/ogp.go
@@ -10,15 +10,21 @@ import (
 
 // OGPTag returns a og: prefixed meta tag
 func OGPTag(property string, value string) string {
-	return `<meta property="og:` + property + `" content="` + value + `" />`
+	return `<meta property="og:` + property + `" content="` + value + `">`
 }
 
 // TwitterTag returns a twitter: prefixed meta tag
 func TwitterTag(property string, value string) string {
-	return `<meta name="twitter:` + property + `" content="` + value + `" />`
+	return `<meta name="twitter:` + property + `" content="` + value + `">`
 }
 
 // SEO returns a skeleton HTML containing only a head tag with SEO metadata
 func SEO(w http.ResponseWriter, r *http.Request, tags ...string) {
+	tags = append([]string{
+		OGPTag("url", r.URL.Scheme+"://"+r.Host),
+		OGPTag("locale", "en"),
+		OGPTag("site_name", "o2"),
+		TwitterTag("card", "summary"),
+	}, tags...)
 	w.Write([]byte(`<!DOCTYPE html><html lang="en"><head>` + strings.Join(tags, "") + `</head></html>`))
 }
